b8fb16a6e1be7140aacd0deb023b66b57856634c
ty2arjit <ty2arjit@gmail.com>
2026-01-24 02:10:07 +0530

    fix : added an action of fetching user

--- mediqueue/lib/actions/patient.actions.ts
+++ mediqueue/lib/actions/patient.actions.ts
@@ -2,6 +2,7 @@
 
 import { ID, Query } from "node-appwrite"
 import { users } from "../appwrite.config"
+import { parseStringify } from "../utils"
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -21,4 +22,14 @@ export const createUser = async (user: CreateUserParams) => {
       return documents?.users[0]
     }
   }
+}
+
+export const getUser = async (userId : string) => {
+  try {
+    const user = await users.get(userId)
+
+    return parseStringify(user)
+  } catch ( error : any ) {
+    console.log(error);
+  } 
 }
