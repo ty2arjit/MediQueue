53af2a33dc1d11d4b43d5dfa589f7c8ffc0cdca7
ty2arjit <ty2arjit@gmail.com>
2026-01-25 01:13:29 +0530

    feat : file uploader ui taken from react-dropzone

---
+++ mediqueue/components/FileUploader.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { convertFileToUrl } from '@/lib/utils'
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import Image from 'next/image'
+
+type FileUploaderProps = {
+  files: File[] | undefined
+  onChange: (files: File[]) => void
+}
+const FileUploader = ({files, onChange}: FileUploaderProps) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    onChange(acceptedFiles)
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}
+    className='file-upload'
+    >
+      <input {...getInputProps()} />
+      {
+        files && files?.length > 0 ? (
+          <Image 
+          src={convertFileToUrl(files[0])}
+          width={1000}
+          height={1000}
+          alt="uploaded image"
+          className='max-h-[400px] overflow-hidden object-cover'
+          />
+        ) : (
+          <>
+          <Image 
+          src="/assets/icons/upload.svg"
+          height={40}
+          width={40}
+          alt="upload"
+          />
+          <div className='file-uplaod_label'>
+            <p className='text-14-regular'>
+              <span className='text-green-500'>Click to Upload</span> or drag and drop
+            </p>
+            <p>SVG, PNG, JPG, GIF (max 800x400)</p>
+          </div>
+          </>
+        )
+
+      }
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
+
+export default FileUploader;
