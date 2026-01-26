49afe305083105dfbf6f6f793ec76a5b9c8320d7
ty2arjit <ty2arjit@gmail.com>
2026-01-27 02:32:46 +0530

    feat : get patient action

--- mediqueue/lib/actions/patient.actions.ts
+++ mediqueue/lib/actions/patient.actions.ts
@@ -77,3 +77,20 @@ export const registerPatient = async ({
     console.error("An error occurred while creating a new patient:", error);
   }
 };
+
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [
+        Query.equal("userId", userId),
+      ]
+    
+    );
+
+    return parseStringify(patients.documents[0]);
+  } catch (error: any) {
+    console.log(error);
+  }
+};
